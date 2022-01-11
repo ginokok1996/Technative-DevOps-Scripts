31a10c2fb5cfc91def7c0158f0c490fb50c835f0
ginokok1996 <ginokok1996@hotmail.com>
2022-01-11 09:51:00 +0100

    added comments  + mounting + create default directories

--- bash/remotePlaybook.sh
+++ bash/remotePlaybook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#define flags
+# Define flags
 while getopts d:f: flag
 do
     case "${flag}" in
@@ -9,35 +9,35 @@ do
     esac
 done
 
-#stop script if destination flag is empty
+# Stop script if destination flag is empty
 if [ -z "$destination" ]
 then
       echo "destination flag is empty";
       exit 1;
 fi
 
-#default to playbook.yml is fileName flag is empty
+# Default to playbook.yml is fileName flag is empty
 if [ -z "$fileName" ]
 then
       echo "fileName flag is empty defaulting to playbook.yml";
       filename="playbook.yml";
 fi
 
-#retrieve the repository name
+# Retrieve the repository name
 NAME=${destination##*/};
 
-#install git cli if not present already
+# Install git cli if not present already
 sudo yum install git -y;
 
-#creates tmp folder for the repository and navigates to it
+# Creates tmp folder for the repository and navigates to it
 mkdir /tmp/playbook;
 cd /tmp/playbook;
 
-#clones the respository to the created tmp folder and navigates to repo
+# Clones the respository to the created tmp folder and navigates to repo
 git clone $destination;
 cd $NAME;
 
-#checks if we can find the file and if so run ansible-playbook with it
+# Checks if we can find the file and if so run ansible-playbook with it
 if test -f "$fileName"; then
     ansible-playbook $fileName;
 else 
@@ -46,5 +46,5 @@ else
     exit 1;
 fi
 
-#remove the temp folder
+# Remove the temp folder
 rm -r /tmp/playbook -f;
